e85834f5843293e6e52b222b26fa16f7eddcc203
Jay <jayjoshi1912007@gmail.com>
2025-02-02 20:14:26 +0530

    side-bar

--- js/script2.js
+++ js/script2.js
@@ -2,29 +2,28 @@ async function getmaterial(folder) {
     // currentfolder = folder;
     let url = `/study/material/${folder}`
     let chapters = await getchapters(folder)
-    
+
     document.querySelector(".cardcontainer").innerHTML = ""
-    
-    for (let i = 1; i < chapters+1; i++) {
-        let geturl = url + `/chapter-${i}` + ".pdf"
+
+    for (let i = 1; i < chapters + 1; i++) {
         document.querySelector(".cardcontainer").innerHTML = document.querySelector(".cardcontainer").innerHTML + `<div class="material">
                     <img src="./img/pdf.png" alt="">
-                        <h3>Chapter-${i}.pdf</h3>
+                        <h3 class="ch">chapter-${i}.pdf</h3>
                     </div>`
-        
-        Array.from(document.querySelector(".material")).forEach(e=>{
-            e.addEventListener("click", async()=>{
-                window.location.href = geturl
-            })
-        })
     }
+
+    Array.from(document.querySelectorAll(".material")).forEach(e => {
+        e.addEventListener("click", async () => {
+            window.location.href = url + "/" + e.querySelector(".ch").innerHTML
+        })
+    })
 }
 
 async function main() {
 
     document.querySelector(".side-bar").addEventListener("click", () => {
         console.log("click");
-        
+
         document.querySelector(".left").style.left = 0
     })
 
@@ -38,11 +37,20 @@ async function main() {
         })
     })
 
+    Array.from(document.getElementsByClassName("card")).forEach(e => {
+            let list = document.querySelector(".subjectlist").getElementsByTagName("ul")[0]
+            list.innerHTML = `<li> <img class="pdfimg" src="img/pdf.png" alt="">
+                            <div class="info">
+                                <div>${e.querySelector(".sub").innerHTML}</div>
+                                <div>All Chapters</div>
+                            </div></li>`
+        
+    })
 }
 
 async function getchapters(folder) {
-    if(folder == "maths"){
-        return 3;
+    if (folder == "maths") {
+        return 1;
     }
 }
 
