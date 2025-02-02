0c8596cc8917d5115b1668a4f4abc1136b5e1c40
Jay <jayjoshi1912007@gmail.com>
2025-02-02 18:21:48 +0530

    donw

---
+++ js/script2.js
@@ -0,0 +1,49 @@
+async function getmaterial(folder) {
+    // currentfolder = folder;
+    let url = `/study/material/${folder}`
+    let chapters = await getchapters(folder)
+    
+    document.querySelector(".cardcontainer").innerHTML = ""
+    
+    for (let i = 1; i < chapters+1; i++) {
+        let geturl = url + `/chapter-${i}` + ".pdf"
+        document.querySelector(".cardcontainer").innerHTML = document.querySelector(".cardcontainer").innerHTML + `<div class="material">
+                    <img src="./img/pdf.png" alt="">
+                        <h3>Chapter-${i}.pdf</h3>
+                    </div>`
+        
+        Array.from(document.querySelector(".material")).forEach(e=>{
+            e.addEventListener("click", async()=>{
+                window.location.href = geturl
+            })
+        })
+    }
+}
+
+async function main() {
+
+    document.querySelector(".side-bar").addEventListener("click", () => {
+        console.log("click");
+        
+        document.querySelector(".left").style.left = 0
+    })
+
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "-100%"
+    })
+
+    Array.from(document.getElementsByClassName("card")).forEach(e => {
+        e.addEventListener("click", async (item) => {
+            material = await getmaterial(`${item.currentTarget.dataset.folder}`);
+        })
+    })
+
+}
+
+async function getchapters(folder) {
+    if(folder == "maths"){
+        return 3;
+    }
+}
+
+main();
